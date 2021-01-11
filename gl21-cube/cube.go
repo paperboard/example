3b105418fb359d79d3e02293f15def5e01bd8afa
Einthusan Vigneswaran <einthusan@paperboardinc.com>
2021-01-11 12:20:09 +0530

    this causes bug when running OpenGL profiler
    https://github.com/golang/go/issues/26504
    using build.Import is not reliable ...
    TODO: must compile resources into go binary

--- gl21-cube/cube.go
+++ gl21-cube/cube.go
@@ -6,7 +6,6 @@
 package main // import "github.com/go-gl/example/gl21-cube"
 
 import (
-	"go/build"
 	"image"
 	"image/draw"
 	_ "image/png"
@@ -205,27 +204,4 @@ func drawScene() {
 	gl.Vertex3f(-1, 1, -1)
 
 	gl.End()
-}
-
-// Set the working directory to the root of Go package, so that its assets can be accessed.
-func init() {
-	dir, err := importPathToDir("github.com/go-gl/example/gl21-cube")
-	if err != nil {
-		log.Fatalln("Unable to find Go package in your GOPATH, it's needed to load assets:", err)
-	}
-	err = os.Chdir(dir)
-	if err != nil {
-		log.Panicln("os.Chdir:", err)
-	}
-}
-
-// importPathToDir resolves the absolute path from importPath.
-// There doesn't need to be a valid Go package inside that import path,
-// but the directory must exist.
-func importPathToDir(importPath string) (string, error) {
-	p, err := build.Import(importPath, "", build.FindOnly)
-	if err != nil {
-		return "", err
-	}
-	return p.Dir, nil
 }
--- gl41core-cube/cube.go
+++ gl41core-cube/cube.go
@@ -7,7 +7,6 @@ package main // import "github.com/go-gl/example/gl41core-cube"
 
 import (
 	"fmt"
-	"go/build"
 	"image"
 	"image/draw"
 	_ "image/png"
@@ -316,27 +315,4 @@ var cubeVertices = []float32{
 	1.0, -1.0, 1.0, 1.0, 1.0,
 	1.0, 1.0, -1.0, 0.0, 0.0,
 	1.0, 1.0, 1.0, 0.0, 1.0,
-}
-
-// Set the working directory to the root of Go package, so that its assets can be accessed.
-func init() {
-	dir, err := importPathToDir("github.com/go-gl/example/gl41core-cube")
-	if err != nil {
-		log.Fatalln("Unable to find Go package in your GOPATH, it's needed to load assets:", err)
-	}
-	err = os.Chdir(dir)
-	if err != nil {
-		log.Panicln("os.Chdir:", err)
-	}
-}
-
-// importPathToDir resolves the absolute path from importPath.
-// There doesn't need to be a valid Go package inside that import path,
-// but the directory must exist.
-func importPathToDir(importPath string) (string, error) {
-	p, err := build.Import(importPath, "", build.FindOnly)
-	if err != nil {
-		return "", err
-	}
-	return p.Dir, nil
 }
